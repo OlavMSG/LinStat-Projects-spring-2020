ca313dfe01a65b86b0f632dd23fa744941bee148
William Scott Grundeland Olsen <olsen.william@outlook.com>
2020-03-15 18:28:10 +0100

    Added subproblem 1d). Finished

--- Problem 1.R
+++ Problem 1.R
@@ -1,21 +1,25 @@
 
-## Subproblem c)
-
 library(nortest)  # Importing package
+library(car)      # Importing package
+
+## Subproblem c)
 
 ds <- read.csv("https://web.stanford.edu/~hastie/CASI_files/DATA/diabetes.csv", sep = ",")
 
-reduced <- lm(prog ~ sex + bmi + map + hdl + ltg - 1, data = ds)  # The five variable reduced model
+reduced <- lm(prog ~ sex + bmi + map + hdl + ltg, data = ds)  # The five variable reduced model
 
 summary(reduced)
 
 plot(reduced$fitted, rstudent(reduced), pch = 20) # Plotting residuals
 qqnorm(rstudent(reduced), pch = 20) # Plotting Q-Q plot
-qqline(rstudent(reduced), col = 2)  # Plotting line in Q-Q plot
+qqline(rstudent(reduced), col = "red")  # Plotting line in Q-Q plot
 
 ad.test(rstudent(reduced))  # Finding p-value, small => reject H_0
 
 
 ## Subproblem d)
 
-#...
+full <- lm(prog ~ ., data = ds) # The full model
+
+# Find the p-value from Pr(>|F|):
+linearHypothesis(full, hypothesis.matrix = c("age = 0", "tc = 0", "ldl = 0", "tch = 0", "glu = 0"))
